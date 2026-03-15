cb2c62c50ec657011cbef0a79a3774e92f7c2603
Gye11 <hilallornekk@gmail.com>
2026-03-16 01:08:38 +0300

    final

--- src/pages/MovieDetailsPage/MovieDetailsPage.jsx
+++ src/pages/MovieDetailsPage/MovieDetailsPage.jsx
@@ -1,37 +1,65 @@
-/* NAVIGATION */
+import { useParams, Link, Outlet, useLocation } from "react-router-dom";
+import { useEffect, useState, useRef } from "react";
+import { getMovieDetails } from "../../services/api";
 
-nav {
-  padding: 15px;
-  border-bottom: 1px solid #ddd;
-  background: white;
-}
+export default function MovieDetailsPage() {
+  const { movieId } = useParams();
+  const location = useLocation();
+  const backLink = useRef(location.state ?? "/movies");
 
-nav a {
-  margin-right: 20px;
-  font-weight: bold;
-  text-decoration: none;
-  color: black;
-}
+  const [movie, setMovie] = useState(null);
 
-nav a:hover {
-  color: red;
-}
+  useEffect(() => {
+    getMovieDetails(movieId).then(setMovie);
+  }, [movieId]);
 
+  if (!movie) return <p>Loading...</p>;
 
-/* MOVIE DETAILS */
+  const posterUrl = movie.poster_path
+    ? `https://image.tmdb.org/t/p/w500${movie.poster_path}`
+    : "https://via.placeholder.com/300x450";
 
-.movieDetails {
-  display: flex;
-  gap: 30px;
-  margin-top: 20px;
-}
+  return (
+    <div>
+      <Link to={backLink.current}>Go back</Link>
 
-.moviePoster {
-  width: 250px;
-}
+      <div className="movieDetails">
+        <img className="moviePoster" src={posterUrl} alt={movie.title} />
 
-.additional {
-  margin-top: 20px;
-  border-top: 1px solid #ddd;
-  padding-top: 10px;
+        <div>
+          <h2>
+            {movie.title} ({movie.release_date?.slice(0, 4)})
+          </h2>
+
+          <p>User Score: {Math.round(movie.vote_average * 10)}%</p>
+
+          <h3>Overview</h3>
+          <p>{movie.overview}</p>
+
+          <h3>Genres</h3>
+          <p>{movie.genres.map((g) => g.name).join(" ")}</p>
+        </div>
+      </div>
+
+      <div className="additional">
+        <h3>Additional information</h3>
+
+        <ul>
+          <li>
+            <Link to="cast" state={location.state}>
+              Cast
+            </Link>
+          </li>
+
+          <li>
+            <Link to="reviews" state={location.state}>
+              Reviews
+            </Link>
+          </li>
+        </ul>
+      </div>
+
+      <Outlet />
+    </div>
+  );
 }
